4678938ead458c49984c9976c5ad4f28c0e527d9
朱亮 <teamide@163.com>
2022-10-19 16:55:30 +0800

    Update init.go

--- db_oracle/init.go
+++ db_oracle/init.go
@@ -7,6 +7,27 @@ import (
 	_ "github.com/godror/godror"
 )
 
+/*
+
+如出现异常：ORA-00000: DPI-1047: Cannot locate a 64-bit Oracle Client library: "The specified module could not be found". See https://oracle.github.io/odpi/doc/installation.html#windows for help
+
+有以下解决方案：
+方案一：
+1、安装oracle 64位客户端
+2、配置环境path，将客户端的目录，比如 c:\oracle\product\11.2.0\client_1\bin 配置到path中
+3、配置环境变量ORACLE_HOME目录设置为如 c:\oracle\product\11.2.0\client_1 后，问题解决。
+
+方案二：
+1、下载轻量级的Oracle客户端： 比如 instantclient-basic-windows.x64-11.2.0.4.0.zip
+2、配置环境变量path：将客户端的目录，比如 c:\oracle\instantclient_11_2 添加到 path
+3、配置环境变量ORACL_HOME： 将客户端的上一级目录 ，比如 c:\oracle 设置为ORACLE_HOME的值
+
+linux方案：
+1、下载轻量级的oracle客户端并解压安装
+2、配置环境变量LD_LIBRARY_PATH=[oracle客户端安装目录]:$LD_LIBRARY_PATH ，NLS_LANG=[oracle数据库NLS_LANGUAGE_NLS_TERRITORY.NLS_CHARACTERSET] ,并添加到 ~/.bash_profile 中
+3、重新登录系统 或 source ~/.bash_profile 使环境变量生效
+*/
+
 func NewDataSourceConfig(user string, password string, host string, port int, sid string) zorm.DataSourceConfig {
 	//connStr := fmt.Sprintf("%s/%s@%s:%d/%s", user, password, host, port, sid)
 	connStr := fmt.Sprintf(`user="%s" password="%s" connectString="%s:%d/%s"`, user, password, host, port, sid)
