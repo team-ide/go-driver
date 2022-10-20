bafa3448b00063839a5d8b6cdd308c12d9a2f2d0
朱亮 <teamide@163.com>
2022-10-20 16:52:42 +0800

    添加ACI头文件引用

---
+++ driver/shentong/go_aci-1.0.8.noarch.publish/drivers/go-aci/c.go
@@ -0,0 +1,6 @@
+package aci
+
+// #cgo CFLAGS: -I ./include
+// #cgo LDFLAGS: -L ./lib/win64
+// #include "aci.go.h"
+import "C"
--- driver/shentong/go_aci-1.0.8.noarch.publish/drivers/go-aci/test/aci_connect_test.go
+++ driver/shentong/go_aci-1.0.8.noarch.publish/drivers/go-aci/test/aci_connect_test.go
@@ -1,4 +1,4 @@
-package aci
+package main
 
 import (
 	"database/sql"
