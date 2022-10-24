4482483cb18a581b7fc3446b85e0190d8827802b
朱亮 <teamide@163.com>
2022-10-24 10:22:16 +0800

    1.0.5，添加arm64

--- driver/shentong/go-aci/a.go
+++ driver/shentong/go-aci/a.go
@@ -1,9 +1,10 @@
 package aci
 
 // #cgo CFLAGS: -Iinclude
-// #cgo loong64 && linux LDFLAGS: -Llib/loongarch64  -laci
-// #cgo mips64 && linux LDFLAGS: -Llib/loongson64  -laci
-// #cgo mips64le && linux LDFLAGS: -Llib/loongson64  -laci
+// #cgo arm64 LDFLAGS: -Llib/arm64  -laci
+// #cgo loong64 LDFLAGS: -Llib/loongarch64  -laci
+// #cgo mips64 LDFLAGS: -Llib/loongson64  -laci
+// #cgo mips64le LDFLAGS: -Llib/loongson64  -laci
 // #cgo amd64 && linux LDFLAGS: -Llib/linux64  -laci
 // #cgo amd64 && windows LDFLAGS: -Llib/win64  -laci
 // #include "aci.go.h"
