28429e717d785633e25fe2c050c1818c900192df
朱亮 <teamide@163.com>
2023-03-13 16:46:27 +0800

    Update a.go

--- driver/shentong/go-aci/a.go
+++ driver/shentong/go-aci/a.go
@@ -5,6 +5,7 @@ package aci
 // #cgo loong64 LDFLAGS: -Llib/loongarch64  -laci
 // #cgo mips64 LDFLAGS: -Llib/loongson64  -laci
 // #cgo mips64le LDFLAGS: -Llib/loongson64  -laci
+// #cgo sw64 LDFLAGS: -Llib/shenwei64  -laci
 // #cgo amd64 && linux LDFLAGS: -Llib/linux64  -laci
 // #cgo amd64 && windows LDFLAGS: -Llib/win64  -laci
 // #cgo amd64 && darwin LDFLAGS: -Llib/darwin  -laci
