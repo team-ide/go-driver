345af507f6e52ddf7a8f0f7ff9d6dc75a6580249
朱亮 <teamide@163.com>
2023-08-10 15:12:31 +0800

    调整 cgo 库引入方式

--- driver/shentong/go-aci/a.go
+++ driver/shentong/go-aci/a.go
@@ -1,13 +1,10 @@
 package aci
 
+/*
 // #cgo CFLAGS: -Iinclude
-// #cgo arm64 LDFLAGS: -Llib/arm64  -laci
-// #cgo loong64 LDFLAGS: -Llib/loongarch64  -laci
-// #cgo mips64 LDFLAGS: -Llib/loongson64  -laci
-// #cgo mips64le LDFLAGS: -Llib/loongson64  -laci
-// #cgo sw64 LDFLAGS: -Llib/shenwei64  -laci
-// #cgo amd64 && linux LDFLAGS: -Llib/linux64  -laci
-// #cgo amd64 && windows LDFLAGS: -Llib/win64  -laci
-// #cgo amd64 && darwin LDFLAGS: -Llib/darwin  -laci
-// #include "aci.go.h"
+#cgo LDFLAGS: -L./lib -laci
+#cgo amd64 && windows LDFLAGS: -Llib/win64  -laci
+#include "aci.go.h"
+
+*/
 import "C"
