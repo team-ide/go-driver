d5f131ff9b4ea651dc35d7456b61e3590aa35e7d
朱亮 <teamide@163.com>
2023-08-10 17:11:01 +0800

    commit

--- driver/shentong/go-aci/a.go
+++ driver/shentong/go-aci/a.go
@@ -2,8 +2,14 @@ package aci
 
 /*
 #cgo CFLAGS: -Iinclude
-#cgo LDFLAGS: -L./lib -laci
-#cgo amd64 && windows LDFLAGS: -L./lib/win64  -laci
+#cgo LDFLAGS: -Llib -laci
+#cgo arm64 LDFLAGS: -Llib/arm64  -laci
+#cgo loong64 LDFLAGS: -Llib/loong64  -laci
+#cgo mips64 LDFLAGS: -Llib/mips64  -laci
+#cgo mips64le LDFLAGS: -Llib/mips64  -laci
+#cgo sw64 LDFLAGS: -Llib/shenwei64  -laci
+#cgo amd64 && linux LDFLAGS: -Llib/amd64  -laci
+#cgo amd64 && windows LDFLAGS: -Llib/win64  -laci
 #include "aci.go.h"
 */
 import "C"
